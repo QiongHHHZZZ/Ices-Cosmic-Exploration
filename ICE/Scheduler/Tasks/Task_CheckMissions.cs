a6b109e6ba30b57f8a90f8fec6ca5d0a942ac8e2
QiongHHHZZZ <158689932@qq.com>
2026-06-07 00:16:26 +0800

    fix: restore mission entry DRTP

--- ICE/Scheduler/Tasks/Task_CheckMissions.cs
+++ ICE/Scheduler/Tasks/Task_CheckMissions.cs
@@ -6,6 +6,7 @@ using ICE.Utilities.Cosmic_Helper;
 using ICE.Utilities.GatheringHelper;
 using ICE.Utilities.GatheringHelper.RouteLoader;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using static ECommons.UIHelpers.AddonMasterImplementations.AddonMaster;
 
@@ -874,6 +875,40 @@ namespace ICE.Scheduler.Tasks
             }
         }
         private static Vector3 randomFishingHole = Vector3.Zero;
+
+        private static bool TryDailyRoutinesTeleportToPersonalReturn(Vector3 destination, string tag)
+        {
+            if (!C.PersonalReturnUseDailyRoutinesTP)
+                return false;
+
+            if (Player.DistanceTo(destination) < 3f)
+                return false;
+
+            if (!Utils.HasPlugin("DailyRoutines"))
+            {
+                if (EzThrottler.Throttle("PersonalReturnMissingDailyRoutines", 8000))
+                    IceLogging.Warning("未检测到 Daily Routines，已回退原有寻路。", tag);
+
+                return false;
+            }
+
+            if (EzThrottler.Throttle("PersonalReturnDailyRoutinesTeleport", 2500))
+            {
+                var command = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "/pdrtp pos {0:F2} {1:F2} {2:F2}",
+                    destination.X,
+                    destination.Y,
+                    destination.Z);
+
+                Svc.Commands.ProcessCommand(command);
+                IceLogging.Debug($"已尝试 Daily Routines 传送：{command}", tag);
+                return true;
+            }
+
+            return false;
+        }
+
         private static bool? CheckForMovementRequired(uint missionId)
         {
             string tag = "[Check Missions: Movement Check]";
@@ -966,17 +1001,37 @@ namespace ICE.Scheduler.Tasks
                         if (Player.DistanceTo(fishingLoc.Value) < 3)
                         {
                             IceLogging.Info($"We have a custom fishing hole set, and we're close to it. {fishingLoc.Value}", tag);
+                            Task_Fishing.MarkMissionEntryPrepared(missionId);
                             randomFishingHole = Vector3.Zero;
                             return true;
                         }
                         else
                         {
+                            if (!Task_Fishing.IsInsideMissionFishingCircle(sheetInfo) &&
+                                !Task_TurninMission.IsInsideTargetCriticalMissionArea(missionId, sheetInfo) &&
+                                Task_Fishing.TryDailyRoutinesTeleportToFishingSpot(fishingLoc.Value, tag))
+                            {
+                                Task_Fishing.MarkMissionEntryPrepared(missionId);
+                                randomFishingHole = Vector3.Zero;
+                                return false;
+                            }
+
                             IceLogging.Verbose($"We have a custom fishing hole set, and we're not within fishing range. Queueing up moving to it: {fishingLoc.Value}");
                             Task_NavmeshMove.Enqueue_NavmeshTask(fishingLoc.Value);
+                            Task_Fishing.MarkMissionEntryPrepared(missionId);
                             randomFishingHole = Vector3.Zero;
                             return true;
                         }
                     }
+                }
+
+                if (Task_Fishing.IsInsideMissionFishingCircle(sheetInfo) ||
+                    Task_TurninMission.IsInsideTargetCriticalMissionArea(missionId, sheetInfo))
+                {
+                    Task_Fishing.MarkMissionEntryPrepared(missionId);
+                    IceLogging.Info("Already inside mission fishing/critical circle, continuing to grab mission", tag);
+                    randomFishingHole = Vector3.Zero;
+                    return true;
                 }
 
                 foreach (var fishingSpot in fishingHole)
@@ -984,6 +1039,7 @@ namespace ICE.Scheduler.Tasks
                     if (Player.DistanceTo(fishingSpot.FishingSpot) < 3)
                     {
                         IceLogging.Info($"We've reached our fishing spot! We are current at: {fishingSpot.FishingSpot}", tag);
+                        Task_Fishing.MarkMissionEntryPrepared(missionId);
                         randomFishingHole = Vector3.Zero;
                         return true;
                     }
@@ -1001,8 +1057,16 @@ namespace ICE.Scheduler.Tasks
                 }
                 else
                 {
+                    if (!Task_TurninMission.IsInsideTargetCriticalMissionArea(missionId, sheetInfo) &&
+                        Task_Fishing.TryDailyRoutinesTeleportToFishingSpot(randomFishingHole, tag))
+                    {
+                        Task_Fishing.MarkMissionEntryPrepared(missionId);
+                        return false;
+                    }
+
                     IceLogging.Verbose("If we've gotten this far, that means we need to figure out a path to go to the node. Doing so now");
                     Task_NavmeshMove.Enqueue_NavmeshTask(randomFishingHole);
+                    Task_Fishing.MarkMissionEntryPrepared(missionId);
                     randomFishingHole = Vector3.Zero;
                     return true;
                 }
@@ -1019,6 +1083,11 @@ namespace ICE.Scheduler.Tasks
                     var territory = Player.Territory.RowId;
                     if (C.CrafterLocations.TryGetValue(territory, out var location))
                     {
+                        if (TryDailyRoutinesTeleportToPersonalReturn(location, tag))
+                        {
+                            return false;
+                        }
+
                         IceLogging.Verbose("If we've gotten this far, that means we need to figure out a path to go to the node. Doing so now");
                         Task_NavmeshMove.Enqueue_NavmeshTask(location);
                         return true;

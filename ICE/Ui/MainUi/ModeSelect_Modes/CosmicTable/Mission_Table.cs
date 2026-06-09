e1013a34daf2356b794cd726a9fd2cda77dec575
QiongHHHZZZ <158689932@qq.com>
2026-06-09 14:53:47 +0800

    fix: prevent mission table row highlight crash

--- ICE/Ui/MainUi/ModeSelect_Modes/CosmicTable/Mission_Table.cs
+++ ICE/Ui/MainUi/ModeSelect_Modes/CosmicTable/Mission_Table.cs
@@ -501,18 +501,6 @@ namespace ICE.Ui.MainUi.ModeSelect_Modes.CosmicTable
             {
                 ImGui.PushID(item.Id);
 
-                var mission = CosmicHelper.CurrentLunarMission;
-
-                if (mission != 0 && mission == item.Id)
-                {
-                    ImGui.TableSetBgColor(ImGuiTableBgTarget.RowBg1, ImGui.GetColorU32(new Vector4(0.0f, 1.0f, 0.2f, 0.25f)));
-                }
-                else if (CosmicHandler.All_AvailableMissions().Contains(item.Id))
-                {
-                    ImGui.TableSetBgColor(ImGuiTableBgTarget.RowBg1, ImGui.GetColorU32(new Vector4(0.0f, 1.0f, 0.2f, 0.25f)));
-                }
-
-
                 bool disabled = C.SelectedMode == ModeSelect.MissionGoldMode
                              || C.SelectedMode == ModeSelect.LevelMode
                              || (C.SelectedMode == ModeSelect.RelicMode && !C.XPRelicOnlyEnabled);
